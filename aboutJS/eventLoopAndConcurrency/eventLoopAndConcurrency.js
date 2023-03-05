d364dd7ccebf20b41f2a89e6d9348f23709bb159
kh-almas <services.almas@gmail.com>
2023-03-06 01:04:08 +0600

    event loop and concurrency

---
+++ aboutJS/eventLoopAndConcurrency/eventLoopAndConcurrency.js
@@ -0,0 +1,28 @@
+/*
+* 39-7
+*/
+function a(){
+    console.log('a')
+    b();
+    console.log('aa')
+}
+
+function b(){
+    console.log('b')
+    c();
+    console.log('bb')
+}
+
+function c(){
+    console.log('c')
+    d();
+    console.log('cc')
+}
+
+function d(){
+    console.log('d');
+    console.log('dd');
+}
+
+a();
+c();
