fbf8bc636b5f1f727efde8af0afc015cd737e630
kh-almas <services.almas@gmail.com>
2023-03-06 14:56:51 +0600

    syntax error

---
+++ errorDebugging/syntaxError/syntaxError.js
@@ -0,0 +1,15 @@
+function isSmall(x)
+{
+    if (x < 10)
+    {
+        return true;
+    }
+    else return false;
+}
+) //extra item
+
+const sum = (a, b) => return a+b; //messing () //unexpected return
+console.log(sum(4, 7))
+
+
+const numbers = [36, 34, 2, 6, 3 5, 7,26, 52] //messing ,
