2433f220324052a0b3dcef227961a9bf577d3bd9
kh-almas <services.almas@gmail.com>
2023-03-13 17:46:47 +0600

    alert confirm prompt

---
+++ browserMilestoneAndDebug/aboutBrowser/alertConfirmPrompt/app.js
@@ -0,0 +1,18 @@
+const showAlert = () => {
+    // show a alert
+    alert('this is alert');
+}
+
+
+const showConfirm = () => {
+    // give ok cancel option
+    const result = confirm('this is confirm');
+    console.log(result);
+}
+
+
+const showPrompt = () => {
+    // give a input field
+    const result = prompt('Type something');
+    console.log(result);
+}
