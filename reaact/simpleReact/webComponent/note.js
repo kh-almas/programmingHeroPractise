56607ca666fa2c4cc781787a84512af589eb7168
kh-almas <services.almas@gmail.com>
2023-03-20 21:10:31 +0600

    type of component

---
+++ reaact/simpleReact/webComponent/note.js
@@ -0,0 +1,8 @@
+/*
+* 4 types of component
+*
+* 1. similar in look different in data
+* 2. Container component (when a component contain repeating component)
+* 3. No pattern, but breakdown for working purpose
+* 4. Stand alone component (a part of website)
+*/
